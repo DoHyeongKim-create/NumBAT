c8ee7fd129ec8d38266e43902884bd265f23ed8e
DoHyeongKim-create <58288714+DoHyeongKim-create@users.noreply.github.com>
2020-01-17 17:10:52 +0900

    Update simo-lit_10-trapezoidal.py

--- lit_examples/simo-lit_10-trapezoidal.py
+++ lit_examples/simo-lit_10-trapezoidal.py
@@ -93,7 +93,7 @@ k_AC = np.real(sim_EM_pump.Eig_values[EM_ival_pump] - sim_EM_Stokes.Eig_values[E
 print('\n AC wavenumber (1/m) = ', np.round(k_AC, 4))
 
 # Calculate Acoustic modes.
-shift_Hz = 7.0*1e9 # select the lowest frequency to start FEM search from.
+shift_Hz = 7.16*1e9 # select the lowest frequency to start FEM search from.
 sim_AC = wguide.calc_AC_modes(num_modes_AC, k_AC, EM_sim=sim_EM_pump, shift_Hz=shift_Hz)
 # # np.savez('wguide_data_AC', sim_AC=sim_AC)
 # npzfile = np.load('wguide_data_AC.npz')
